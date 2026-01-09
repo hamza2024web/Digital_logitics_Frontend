257866c311f538c58f01df34c1bec7956fd852d7
hamza2024web <hamzaakroubi1528@gmail.com>
2026-01-09 15:53:03 +0100

    create the dashboard of admin

---
+++ src/app/features/warehouse/dashboard/warehouse-dashboard/warehouse-dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warehouse-dashboard',
+  imports: [],
+  templateUrl: './warehouse-dashboard.html',
+  styleUrl: './warehouse-dashboard.scss',
+})
+export class WarehouseDashboard {
+
+}

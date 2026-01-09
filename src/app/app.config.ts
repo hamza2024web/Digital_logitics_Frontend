d571cb8af1397a4d215707f5d645d7436bfc1502
hamza2024web <hamzaakroubi1528@gmail.com>
2026-01-09 15:18:10 +0100

    update the app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import {errorInterceptor} from './core/interceptors/error.interceptor';
+import {refreshTokenInterceptor} from './core/interceptors/refresh-token.interceptor';
+import {authInterceptor} from './core/interceptors/auth.interceptor';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {routes} from './app.routes';
+import {provideRouter} from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideZoneChangeDetection({ eventCoalescing: true}),
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([
+        authInterceptor,
+        refreshTokenInterceptor,
+        errorInterceptor
+      ])
+    )
   ]
 };

0b808796f29c9598e78e83c12fd4986a01a6af51
hamza2024web <hamzaakroubi1528@gmail.com>
2026-01-15 18:58:50 +0100

    finish the implementations of warehouses managements

--- src/app/features/admin/warehouses/warehouse-list/warehouse-list.ts
+++ src/app/features/admin/warehouses/warehouse-list/warehouse-list.ts
@@ -1,11 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {Warehouse} from '../../../../api/models/warehouse.model';
 import {WarehouseApiService} from '../../../../api/models/warehouse-api.service';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-warehouse-list',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './warehouse-list.html',
   styleUrl: './warehouse-list.scss',
 })

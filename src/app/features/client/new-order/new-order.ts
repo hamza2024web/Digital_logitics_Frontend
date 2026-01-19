41df1ef1dc0374e3676e9bd3798f1558c3955b7d
hamza2024web <hamzaakroubi1528@gmail.com>
2026-01-19 17:14:05 +0100

    create order details component

---
+++ src/app/features/client/new-order/new-order.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-order',
+  imports: [],
+  templateUrl: './new-order.html',
+  styleUrl: './new-order.scss',
+})
+export class NewOrder {
+
+}

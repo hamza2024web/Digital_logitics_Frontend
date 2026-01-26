3856daea9a19a2f697aa7a2eeec765f292059247
hamza2024web <hamzaakroubi1528@gmail.com>
2026-01-26 09:13:07 +0100

    install the ngrx framework

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,19 @@ import {authInterceptor} from './core/interceptors/auth.interceptor';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {routes} from './app.routes';
 import {provideRouter} from '@angular/router';
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, isDevMode, provideZoneChangeDetection} from '@angular/core';
+import {provideStoreDevtools} from '@ngrx/store-devtools';
+import {provideEffects} from '@ngrx/effects';
+import {provideStore} from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideStore(),
+    provideEffects([]),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+    }),
     provideZoneChangeDetection({ eventCoalescing: true}),
     provideRouter(routes),
     provideHttpClient(

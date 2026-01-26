9ad931f790070bb9672c0743c3e171446205a012
hamza2024web <hamzaakroubi1528@gmail.com>
2026-01-26 13:15:52 +0100

    update the action product

--- src/app/store/products/product.actions.ts
+++ src/app/store/products/product.actions.ts
@@ -1,12 +1,12 @@
-import {ProductQuery, ProductResponse} from '../../api/models/product.model';
 import {createActionGroup, props} from '@ngrx/store';
+import {ProductQuery, ProductResponse} from '../../api/models/product.model';
 
 export const ProductsActions = createActionGroup({
   source: 'Products',
-  events: {
-    'Load Products': props<{ query: ProductQuery }>(),
-    'Load Products Success': props<{ response: ProductResponse}>(),
-    'Load Products Failure': props<{ error:any }>(),
-    'Set Query': props<{ partialQuery : Partial<ProductQuery> }>(),
+  events : {
+    'Load Products' : props<{ query: ProductQuery }>(),
+    'Load Products Success' : props<{ response: ProductResponse}>(),
+    'Load Products Failure' : props<{ error:any }>(),
+    'Set Query' : props<{ partialQuery : Partial<ProductQuery> }>()
   }
-});
+})
